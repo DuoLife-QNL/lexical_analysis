9aee629cbf8b5a3c57aecaa2505e615a0e69ca52
Ethan <Ethan_Lee@bupt.edu.cn>
2019-10-11 23:42:25 +0800

    creat identifier table without map() function

--- token.cpp
+++ token.cpp
@@ -1,3 +1,31 @@
 #include"token.h"
 
 int token::count = 0;
+
+void token::inc_cnt(){
+    count++;
+}
+
+int token::get_cnt(){
+    return count;
+}
+
+void id::add_id(char* s){
+    list.push_back(string(s));
+}
+
+void num::display(char* s){
+    cout << "Number" << setw(20) << s << endl;
+}
+
+void operators::display(char* s){
+    cout << "Operator" << setw(20) << s << endl;    
+}
+
+void delimiter::display(char* s){
+    cout << "Delimiter" << setw(20) << s << endl;
+}
+
+void keyword::display(char* s){
+    cout << "Keyword" << setw(20) << s << endl;
+}
